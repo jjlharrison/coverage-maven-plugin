4f3a340ba18e12224b289f9319ae4f3281fedcd8
James Harrison <james.harrison@cognitran.com>
2019-01-22 07:43:07 +0000

    COVR-4 Remove unused import.

--- src/main/java/com/cognitran/products/coverage/changes/ChangeCoverageMojo.java
+++ src/main/java/com/cognitran/products/coverage/changes/ChangeCoverageMojo.java
@@ -14,7 +14,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.Reader;
-import java.nio.charset.StandardCharsets;
 import java.text.DecimalFormat;
 import java.util.List;
 import java.util.function.ToIntFunction;
